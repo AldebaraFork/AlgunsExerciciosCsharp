dd662af5a6ed374c0021d82d8ca8fab53e0f80d6
eduardo <eduardoamorim758@proton.me>
2024-02-17 19:51:11 -0300

    Meu primeiro teste com o Git e treino de logica com c#

---
+++ Git-Logica/TesteGiteLogica/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.Write("Digite o valor de A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Digite o valor de B: ");
+double B = double.Parse(Console.ReadLine());
+double C = (A * B) / 2;
+
+Console.WriteLine("O do resultado da multiplicação de A por B dividido por 2 é: " + C);

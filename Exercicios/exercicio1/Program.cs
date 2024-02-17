4e510c6319803f8216d51cd6ba81c0e9194f0a68
eduardo <eduardoamorim758@proton.me>
2024-02-17 20:44:39 -0300

    Primeiro exercicio de Lógica resolvido

---
+++ Exercicios/exercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor de A: ");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de B: ");
+        double B = double.Parse(Console.ReadLine());
+
+        double C = (A + B * 2) / 5;
+
+        Console.WriteLine($"O resultado da multiplicação de A por B dividido por 2 é: " + C);
+    }
+}

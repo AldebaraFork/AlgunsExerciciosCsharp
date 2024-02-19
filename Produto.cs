676dad8e8700c7c97ded06f7e52483f02421ac5a
Eduardo Pereira <145592635+AldebaraFork@users.noreply.github.com>
2024-02-19 16:36:46 -0300

    Update Produto.cs

--- Produto.cs
+++ Produto.cs
@@ -4,5 +4,9 @@ public class Class1
 {
 	public Class1()
 	{
-	}
+		int A = int.Parse(Console.ReadLine());
+                int B = int.Parse(Console.ReadLine());
+                int PROD = A * B / 2;
+                Console.WriteLine("PROD = " + PROD);
+	} 
 }

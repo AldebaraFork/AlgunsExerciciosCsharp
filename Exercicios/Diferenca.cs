38b5d34038fc5070bf75cddd5d8ac1f1cea658c4
eduardo <eduardoamorim758@proton.me>
2024-02-19 17:24:58 -0300

    Teste de Git

--- Exercicios/Diferenca.cs
+++ Exercicios/Diferenca.cs
@@ -6,6 +6,7 @@ public class Class1
 	{
 		Console.WriteLine("Digite 4 valores inteiros");
 
+<<<<<<< HEAD
 		Console.WriteLine("Digite o valor de A");
 		int A = int.parse(Console.ReadLine());
 		Console.WriteLine("Digite o valor de B");
@@ -15,4 +16,15 @@ public class Class1
 		int DIFERENCA = (A * B - C * D).
 	    Console.WriteLine("DIFERENÇA = " + DIFERENCA);
     }
+=======
+                Console.WriteLine("Digite o valor de A");
+                int A = int.parse(Console.ReadLine());
+                Console.WriteLine("Digite o valor de B");
+                int B = int.parse(Console.ReadLine());
+                Console.WriteLine("Digite o valor de C");
+                int C = int.parse(Console.ReadLine());
+                int DIFERENCA = (A * B - C * D).
+                Console.WriteLine("DIFERENÇA = " + DIFERENCA);
+	}
+>>>>>>> 3dadb0d469a2f01b2ffaf3a729657878b4445316
 }

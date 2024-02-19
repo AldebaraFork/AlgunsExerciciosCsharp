1a3bb1036ff1c5b38e8c7418b86e6cc2e377bbab
eduardo <eduardoamorim758@proton.me>
2024-02-19 16:42:37 -0300

    Adiçao de comentarios e calc de media

---
+++ Exercicios/ExercicioSomaSimples.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		int A = int.parse(Console.Readline());
+		int B = int.parse(Console.ReadLine());
+		int SOMA = A + B;
+		Console.WriteLine("SOMA = " + SOMA);
+
+	}
+}
---
+++ Exercicios/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		int A = int.Parse(Console.ReadLine());
+		int B = int.Parse(Console.ReadLine());
+		int PROD = A * B / 2;
+		Console.WriteLine("PROD = " + PROD);
+	}
+}

8063e5e5fc9815f7ed131b11692e50c7c1de14a2
eduardo <eduardoamorim758@proton.me>
2024-02-19 17:20:35 -0300

    Calculo de salario e att na Diferença

--- Exercicios/Diferenca.cs
+++ Exercicios/Diferenca.cs
@@ -4,5 +4,15 @@ public class Class1
 {
 	public Class1()
 	{
-	}
+		Console.WriteLine("Digite 4 valores inteiros");
+
+		Console.WriteLine("Digite o valor de A");
+		int A = int.parse(Console.ReadLine());
+		Console.WriteLine("Digite o valor de B");
+		int B = int.parse(Console.ReadLine());
+		Console.WriteLine("Digite o valor de C");
+		int C = int.parse(Console.ReadLine());
+		int DIFERENCA = (A * B - C * D).
+	    Console.WriteLine("DIFERENÇA = " + DIFERENCA);
+    }
 }
---
+++ Exercicios/Salario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.globalization;
+public class Class1
+{
+	public Class1()
+	{
+		Console.WriteLine("Digite o número de funcionários: ");
+		int Funcionarios = int.parse.Console.ReadLine();
+		Console.WriteLine("Digite o número de horas trabalhadas: ");
+		int HorasTrabalhadas = int.parse.Console.ReadLine();
+	    Console.writeLine("Digite o valor da hora trabalhada: ");
+		double ValorHora = double.parse.toString(Console.ReadLine());
+        
+		double Salario = HorasTrabalhadas * ValorHora;
+
+		Console.WriteLine("Número de funcionários: " + Funcionarios);
+		Console.WriteLine("Salário: " + Salario.ToString("F2", CultureInfo.CurrentCulture));
+	}
+}

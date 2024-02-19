b9315cff3610234386109e5719449ceec032b7f7
eduardo <eduardoamorim758@proton.me>
2024-02-19 16:32:53 -0300

    Mudanças no exercicio de soma e calc PRODUTO

--- ExercicioSomaSimples.cs
+++ ExercicioSomaSimples.cs
@@ -4,5 +4,10 @@ public class Class1
 {
 	public Class1()
 	{
+		int A = int.parse(Console.Readline());
+		int B = int.parse(Console.ReadLine());
+		int SOMA = A + B;
+		Console.WriteLine("SOMA = " + SOMA);
+
 	}
 }

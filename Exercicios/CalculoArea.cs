6da8d7c87182ec83a25b76e770fb0c1e0c280824
eduardo <eduardoamorim758@proton.me>
2024-02-20 15:55:43 -0300

    Exercicio para calculo de area simples

---
+++ Exercicios/CalculoArea.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{ 
+        //tipando as variaveis
+		double b, be, h, area
+
+        //atribuição dos valores
+        b = 6.0;
+        be = 8.0;
+        h = 5.0;
+
+        //expressão
+        area = (b + be) / 2.0 * h;
+        Console.Writeline(area);
+
+        //para um codigo mais complexo atribuir as variaves com double.parse(Console.Readline());
+        //voce tambem pode usar a mesma formula para varias formas geometricas diferentes
+    }
+}

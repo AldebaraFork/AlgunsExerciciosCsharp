41f390c3553c2f304d26d96421b19047ea654369
eduardo <eduardoamorim758@proton.me>
2024-02-20 17:06:16 -0300

    Declarando variaveis e pedindo entrada de dados

---
+++ Exercicios/DeclarandoVariaveis.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Remoting.Messaging;
+
+public class Program
+{
+    public static void main()
+    {
+        int idade = int.Parse(Console.ReadLine());
+        if (idade <= 18)
+        {
+            Console.WriteLine("Peça autorização de um Adulto");
+        }
+        else
+        {
+            Console.WriteLine("Bem vindo!");
+        }
+
+        Console.WriteLine("Por favor, informe o seu salario atual!");
+        double salario = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+        Console.WriteLine("O valor inserido é de R$: " + salario);
+
+        Console.WriteLine("Insira sua altura ");
+        double altura = double.Parse(Console.ReadLine());
+        if (altura < 1.70)
+        {
+            Console.Write("Uau voce é uma pessoa classificada baixa");
+        }
+        else
+        {
+            Console.Write("Voce é uma pessoa classificada alta!");
+        }
+
+        Console.WriteLine("Porfavor informe a primeira letra do seu genero M ou F");
+        char genero = char.Parse(Console.ReadLine());
+        Console.WriteLine("Seu genero é: " + genero);
+
+        Console.WriteLine("Por favor, insira seu nome");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Olá," + nome + " Seja bem vindo!");
+
+
+
+
+    }
+}

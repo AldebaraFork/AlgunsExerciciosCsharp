c1d02b52383686d77fbefa82a7fc2eeba90ae12a
eduardo <eduardoamorim758@proton.me>
2024-02-20 22:07:33 -0300

    exercicio de operadores logicos e comparativos vou fazer amanha

--- Exercicios/CalculoArea.cs
+++ Exercicios/CalculoArea.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 
-public class Class1
-{
-    public Class1()
+
+    public class Area
     {
-        //tipando as variaveis
-        double b, be, h, area
+        public static void CalculandoArea()
+        {
+            //tipando as variaveis
+            double b, be, h, area;
 
-        //atribuição dos valores
-        b = 6.0;
-        be = 8.0;
-        h = 5.0;
+            //atribuição dos valores
+            b = 6.0;
+            be = 8.0;
+            h = 5.0;
 
-        //expressão
-        area = (b + be) / 2.0 * h;
-        Console.Writeline(area);
+            //expressão
+            area = (b + be) / 2.0 * h;
+            Console.WriteLine(area);
 
-        //para um codigo mais complexo atribuir as variaves com double.parse(Console.Readline());
-        //voce tambem pode usar a mesma formula para varias formas geometricas diferentes
+            //para um codigo mais complexo atribuir as variaves com double.parse(Console.Readline());
+            //voce tambem pode usar a mesma formula para varias formas geometricas diferentes
+        }
     }
-}
--- Exercicios/Diferenca.cs
+++ Exercicios/Diferenca.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 
-public class Class1
+
+namespace CalculoDiferenca
 {
-	public Class1()
-	{
-		Console.WriteLine("Digite 4 valores inteiros");
+    public class Diferenca
+    {
+        public static void CalculandoDiferenca()
+        {
+            Console.WriteLine("Digite 4 valores inteiros");
 
 
-		Console.WriteLine("Digite o valor de A");
-		int A = int.parse(Console.ReadLine());
-		Console.WriteLine("Digite o valor de B");
-		int B = int.parse(Console.ReadLine());
-		Console.WriteLine("Digite o valor de C");
-		int C = int.parse(Console.ReadLine());
-		int DIFERENCA = (A * B - C * D).
-	    Console.WriteLine("DIFERENÇA = " + DIFERENCA);
+            Console.WriteLine("Digite o valor de A");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de C");
+            int C = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de D");
+            int D = int.Parse(Console.ReadLine());
+            int DIFERENCA = (A * B - C * D);
+            Console.WriteLine("DIFERENÇA = " + DIFERENCA);
+        }
     }
+}
